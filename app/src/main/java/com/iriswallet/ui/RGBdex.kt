15819558734c428b6a8a7afb0867b8450aed3030
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-01-17 07:02:34 -0300

    Update RGBdex.kt

--- app/src/main/java/com/iriswallet/ui/RGBdex.kt
+++ app/src/main/java/com/iriswallet/ui/RGBdex.kt
@@ -1,1 +1,11 @@
+package com.iriswallet.ui
 
+import android.os.Bundle
+import android.text.util.Linkify
+import android.view.View
+import com.iriswallet.BuildConfig
+import com.iriswallet.R
+import com.iriswallet.databinding.FragmentAboutPageBinding
+import com.iriswallet.utils.AppConstants
+import com.iriswallet.utils.AppContainer
+import java.util.regex.Pattern
