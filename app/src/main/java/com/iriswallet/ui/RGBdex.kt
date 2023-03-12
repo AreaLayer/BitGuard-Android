e46f5ec0b28e871786bb2b2f63acebbe0f383aec
223880 <rafael.segala45@gmail.com>
2023-03-12 10:22:01 -0300

    Updates from App
    UI added from BitDEX
    
    Build gradle update

--- app/src/main/java/com/iriswallet/ui/RGBdex.kt
+++ app/src/main/java/com/iriswallet/ui/RGBdex.kt
@@ -9,3 +9,35 @@ import com.iriswallet.databinding.FragmentAboutPageBinding
 import com.iriswallet.utils.AppConstants
 import com.iriswallet.utils.AppContainer
 import java.util.regex.Pattern
+
+// Define the swap token interface
+interface TokenSwap {
+    // The address of the token being swapped
+    var tokenAddress: String
+
+    // The amount of the token being swapped
+    var tokenAmount: Float
+
+    // The address of the token to swap for
+    var swapTokenAddress: String
+
+    // The amount of the token to receive in exchange
+    var swapTokenAmount: Float
+}
+
+// Define the user interface for the token swap feature
+fun main() {
+    // Get the input from the user
+    val swap = TokenSwap(
+        tokenAddress = input("Enter the address of the token to swap: "),
+        tokenAmount = inputFloat("Enter the amount of the token to swap: "),
+        swapTokenAddress = input("Enter the address of the token to swap for: "),
+        swapTokenAmount = inputFloat("Enter the amount of the token to receive in exchange: ")
+    )
+
+    // Perform the token swap
+    // ...
+
+    // Display the results to the user
+    display("Token swap complete!")
+}
